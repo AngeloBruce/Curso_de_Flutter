c611cfcfff734fe335a46da0f3765e20b4f7f5c6
Angelo Bruce <angelobruce1204@gmail.com>
2021-08-16 21:24:39 -0600

    Asiendo la api

--- componentes/lib/main.dart
+++ componentes/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:componentes/src/pages/home_page.dart';
-import 'package:componentes/src/pages/home_temp.dart';
 import 'package:flutter/material.dart';
 // Hecho por your
-import 'package:componentes/src/pages/home_temp.dart' show HomePageTemp;
 
 void main() => runApp(MyApp());
 
