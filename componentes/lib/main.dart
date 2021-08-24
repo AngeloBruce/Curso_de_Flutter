fdb82a6664948c2bae175cf66288d192ac4318f9
Angelo Bruce <angelobruce1204@gmail.com>
2021-08-23 20:58:20 -0600

    Componentes

--- componentes/lib/main.dart
+++ componentes/lib/main.dart
@@ -1,15 +1,1 @@
-import 'package:componentes/src/pages/home_page.dart';
-import 'package:flutter/material.dart';
-// Hecho por your
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Componentes APP',
-        debugShowCheckedModeBanner: false,
-        home: HomePage());
-  }
-}
